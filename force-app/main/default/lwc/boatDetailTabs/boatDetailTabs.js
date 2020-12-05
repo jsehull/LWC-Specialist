76559f5e8744f05de0a1d502383f6e521ffe4cae
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2020-12-04 22:32:35 -0300

    Adicionando custom labels e html inicial

--- force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
+++ force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
@@ -1,19 +1,18 @@
 import { LightningElement } from 'lwc';
 
 // Custom Labels Imports
-// import labelDetails for Details
-// import labelReviews for Reviews
-// import labelAddReview for Add_Review
-// import labelFullDetails for Full_Details
-// import labelPleaseSelectABoat for Please_select_a_boat
+import labelDetails from '@salesforce/label/c.Details';
+import labelReviews from '@salesforce/label/c.Reviews';
+import labelAddReview from '@salesforce/label/c.Add_Review';
+import labelFullDetails from '@salesforce/label/c.Full_Details';
+import labelPleaseSelectABoat from '@salesforce/label/c.Please_select_a_boat';
+
 // Boat__c Schema Imports
-// import BOAT_ID_FIELD for the Boat Id
-// import BOAT_NAME_FIELD for the boat Name
+import BOAT_ID_FIELD from '@salesforce/schema/Boat__c.Id';
+import BOAT_NAME_FIELD from '@salesforce/schema/Boat__c.Name';
 const BOAT_FIELDS = [BOAT_ID_FIELD, BOAT_NAME_FIELD];
 
 export default class BoatDetailTabs extends LightningElement {
-    boatId;
-    wiredRecord;
     label = {
         labelDetails,
         labelReviews,
@@ -21,6 +20,9 @@ export default class BoatDetailTabs extends LightningElement {
         labelFullDetails,
         labelPleaseSelectABoat,
     };
+    boatId;
+
+    wiredRecord = { data: [] };
 
     // Decide when to show or hide the icon
     // returns 'utility:anchor' or null
